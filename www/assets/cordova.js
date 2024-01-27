c06a58e401bc2219eb270092f8461454dcd145c5
Julian Gabriel <juliangabriel570@gmail.com>
2024-01-27 08:49:44 +0100

    Add Cordova.js script tag to index.html

---
+++ www/assets/cordova.js
@@ -0,0 +1,21 @@
+function onLoad() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+}
+
+function onDeviceReady() {
+    document.addEventListener("pause", onPause, false);
+    document.addEventListener("resume", onResume, false);
+    document.addEventListener("menubutton", onMenuKeyDown, false);
+}
+
+function onPause() {
+}
+
+function onResume() {
+}
+
+function onMenuKeyDown() {
+}
+
+window.addEventListener("cordovacallbackerror", function (event) {
+});
